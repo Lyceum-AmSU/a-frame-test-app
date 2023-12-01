04d9ba2cc138704e9ed8bfa4f4f7213d6946bc52
epidersis <32263760+epidersis@users.noreply.github.com>
2023-12-01 09:29:02 +0900

    Initial commit

---
+++ .svelte-kit/generated/client/nodes/3.js
@@ -0,0 +1,1 @@
+export { default as component } from "../../../../src/routes/a-frame/+page.svelte";
---
+++ src/routes/a-frame/+page.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+    import 'aframe';
+	
+	let box = '#4CC3D9';
+	let sphere = '#EF2D5E';
+	let cylinder = '#FFC65D';
+</script>
+
+<div class="controls">
+	<label>
+		<input type="color" bind:value={box}>	
+		box
+	</label>
+	
+	<label>
+		<input type="color" bind:value={sphere}>	
+		sphere
+	</label>
+	
+	<label>
+		<input type="color" bind:value={cylinder}>	
+		cylinder
+	</label>
+</div>
+
+<a-scene background="color: #FAFAFA">
+	<a-box position="-1 0.5 -3" rotation="0 45 0" color={box} shadow></a-box>
+	<a-sphere position="0 1.25 -5" radius="1.25" color={sphere} shadow></a-sphere>
+	<a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color={cylinder} shadow></a-cylinder>
+	<a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4" shadow></a-plane>
+</a-scene>
+
+<style>
+	.controls {
+		position: absolute;
+		top: 1em;
+		left: 1em;
+		z-index: 2;
+	}
+	
+	input {
+		height: 2em;
+	}
+</style>
